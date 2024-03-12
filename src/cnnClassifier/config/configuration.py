637bd11035527a98a7c553409bae0057d1d15184
Vedansh1857 <vedanshgupta606@gmail.com>
2024-03-12 22:24:54 +0530

    The base model prepared

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,7 @@
 from src.cnnClassifier.constants import *
 from src.cnnClassifier.utils.common import read_yaml, create_directories
 from src.cnnClassifier.entity.config_entity import DataIngestionConfig
+from src.cnnClassifier.entity.config_entity import PrepareBaseModelConfig
 
 class ConfigurationManager:
     def __init__(self, config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH):
@@ -22,3 +23,21 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+
+        create_directories([config.root_dir])
+
+        base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_classes=self.params.CLASSES,
+            params_weights=self.params.WEIGHTS,
+            params_learning_rate=self.params.LEARNING_RATE
+        )
+
+        return base_model_config
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -7,3 +7,14 @@ class DataIngestionConfig:
     source_url: str
     local_data_file: Path
     unzip_dir: Path
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_include_top: bool
+    params_classes: int
+    params_weights: str
+    params_learning_rate: float
