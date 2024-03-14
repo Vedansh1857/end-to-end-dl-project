938931f5eb8debc6a4a827536d8bd489c4a5f4fb
Vedansh1857 <vedanshgupta606@gmail.com>
2024-03-14 22:43:03 +0530

    Model training completed

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,9 @@
+import os
 from src.cnnClassifier.constants import *
 from src.cnnClassifier.utils.common import read_yaml, create_directories
 from src.cnnClassifier.entity.config_entity import DataIngestionConfig
 from src.cnnClassifier.entity.config_entity import PrepareBaseModelConfig
+from src.cnnClassifier.entity.config_entity import ModelTrainingConfig
 
 class ConfigurationManager:
     def __init__(self, config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH):
@@ -41,3 +43,24 @@ class ConfigurationManager:
         )
 
         return base_model_config
+    
+    def get_model_training_config(self) -> ModelTrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir,"kidney-ct-scan-image")
+
+        create_directories([Path(training.root_dir)])
+
+        model_training_config = ModelTrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return model_training_config
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -18,3 +18,16 @@ class PrepareBaseModelConfig:
     params_classes: int
     params_weights: str
     params_learning_rate: float
+
+@dataclass(frozen=True)
+class ModelTrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
+
+    
